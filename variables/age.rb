535df493054ab5728bed67b673b31889e87c2cc0
Michael Short <mikeshort76@hotmail.com>
2020-08-16 17:26:58 -0400

    Adding variables

---
+++ variables/age.rb
@@ -0,0 +1,6 @@
+puts "How old are you?"
+age = gets.chomp
+puts "In 10 years, you will be #{age.to_i + 10} years old!"
+puts "In 20 years, you will be #{age.to_i + 20} years old!"
+puts "In 30 years, you will be #{age.to_i + 30} years old!"
+puts "In 40 years, you will be #{age.to_i + 40} years old!"
