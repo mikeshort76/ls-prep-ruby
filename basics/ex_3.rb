aac98898e738ad7fd57623a7559be92260423cc6
Michael Short <mikeshort76@hotmail.com>
2020-08-16 16:41:30 -0400

    Adding basics folder

---
+++ basics/ex_3.rb
@@ -0,0 +1,8 @@
+titles = {  "Shawshank_Redemption": 1994,
+            "Star Trek: TMP": 1979,
+            "Along Came A Spider": 2001
+          }
+
+puts titles["Shawshank_Redemption"]
+puts titles["Star Trek: TMP"]
+puts titles["Along Came A Spider"]
---
+++ basics/ex_4.rb
@@ -0,0 +1,5 @@
+titles = [1994, 1979, 2001]
+          
+puts titles[0]
+puts titles[1]
+puts titles[2]
